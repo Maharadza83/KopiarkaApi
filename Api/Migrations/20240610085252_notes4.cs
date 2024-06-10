4e8c89ade32a154f333b2deaae823df62a75288d
Igor <Igor.patkowski7@gmail.com>
2024-06-10 10:54:59 +0200

    spadła baza a potem wstała

---
+++ Api/Migrations/20240610085252_notes4.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class notes4 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Notes",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Author = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreationDate = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Content = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Notes", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Notes");
+        }
+    }
+}
--- Api/Model/Entities/Note/Note.cs
+++ Api/Model/Entities/Note/Note.cs
@@ -2,10 +2,12 @@
 {
     public class Note
     {
-        public string Id { get; set; } 
+        public string Id { get; set; }
         public string Author { get; set; }
         public string CreationDate { get; set; } = DateTime.UtcNow.ToString("yyyy-MM-dd");
         public string Name { get; set; } = string.Empty;
         public string Content { get; set; } = string.Empty;
+//        public byte[] FileData { get; set; }
+ //       public string FileName { get; set; }
     }
 }
