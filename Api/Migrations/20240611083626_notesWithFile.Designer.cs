d57c6ecc70e78417c69db4254b3195641fadb6c1
Igor <Igor.patkowski7@gmail.com>
2024-06-11 11:45:01 +0200

    File Sending

--- Api/Migrations/20240611083626_notesWithFile.Designer.cs
+++ Api/Migrations/20240611083626_notesWithFile.Designer.cs
@@ -3,6 +3,7 @@ using Api.Model.Entities.Note;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Api.Migrations
 {
     [DbContext(typeof(NoteContext))]
-    partial class NoteContextModelSnapshot : ModelSnapshot
+    [Migration("20240611083626_notesWithFile")]
+    partial class notesWithFile
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
